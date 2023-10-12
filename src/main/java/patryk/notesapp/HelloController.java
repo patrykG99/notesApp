c9b3a654ca123c65641702783ed2423dc8db5cf9
qxeer <qxeer84@gmail.com>
2023-10-13 00:13:17 +0200

    added notes and ability to move them between different columns

--- src/main/java/patryk/notesapp/HelloController.java
+++ src/main/java/patryk/notesapp/HelloController.java
@@ -1,17 +1,102 @@
 package patryk.notesapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class HelloController {
 
-    @FXML
-    private VBox doneCoulmn;
+
 
     @FXML
-    private VBox inProgressColumn;
+    private Button addToDo;
 
     @FXML
-    private VBox toDoColumn;
+    private VBox toDoBox;
+
+
+
+    @FXML
+    private VBox inProgressBox;
+
+    @FXML
+    void addToDoNote(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Note.fxml"));
+            Node note = loader.load();
+            toDoBox.getChildren().add(note);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    void initialize() {
+        setupDragAndDrop(toDoBox);
+        setupDragAndDrop(inProgressBox);
+    }
+
+    private void setupDragAndDrop(VBox box) {
+        box.setOnDragDetected(event -> {
+            Node node = (Node) event.getTarget();
+            if (node != null && node.getParent().equals(box)) {
+                SnapshotParameters sp = new SnapshotParameters();
+                WritableImage snapshot = node.snapshot(sp, null);
+                node.setStyle("");
+                node.setVisible(false);
+                Dragboard db = node.startDragAndDrop(TransferMode.MOVE);
+                ClipboardContent content = new ClipboardContent();
+                content.putString("Note");
+
+                db.setDragView(snapshot);
+                db.setDragViewOffsetX(event.getX());
+                db.setDragViewOffsetY(event.getY());
+                db.setContent(content);
+                event.consume();
+            }
+        });
+
+        box.setOnDragOver(event -> {
+            if (event.getDragboard().hasString()) {
+                event.acceptTransferModes(TransferMode.MOVE);
+            }
+            event.consume();
+        });
+
+        box.setOnDragDropped(event -> {
+            Node node = (Node) event.getGestureSource();
+            VBox sourceBox = (VBox) node.getParent();
+            if (!sourceBox.equals(box)) {
+                sourceBox.getChildren().remove(node);
+                box.getChildren().add(node);
+            }
+            node.setVisible(true);
+            event.setDropCompleted(true);
+            event.consume();
+        });
+
+        box.setOnDragDone(event -> {
+            Node node = (Node) event.getGestureSource();
+            if (event.getTransferMode() != TransferMode.MOVE) {
+                node.setVisible(true);
+            }
+        });
+    }
+
+
 
 }
---
+++ src/main/java/patryk/notesapp/NoteController.java
@@ -0,0 +1,11 @@
+package patryk.notesapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class NoteController {
+    @FXML
+    void handleDrag(MouseEvent event) {
+        System.out.println("dsada");
+    }
+}
